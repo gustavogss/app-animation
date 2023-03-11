9c511da36b8ccc84d2c5dfd8fc016f97c446c337
gustavogss <gustavogss.dev@gmail.com>
2023-03-10 23:36:56 -0300

    Zoom animation

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import { View, Text, Button } from 'react-native';
 import { styles } from './styles';
-import Animated, { useAnimatedStyle, useSharedValue }  from 'react-native-reanimated';
+import Animated, { 
+  Easing,
+  useAnimatedStyle, 
+  useSharedValue, 
+  withTiming,
+  withSpring
+ }  
+  from 'react-native-reanimated';
 
 export function Home() {
 
@@ -13,14 +20,17 @@ export function Home() {
   }));
 
 function handlerAnimated() {
-  (scale.value === 1) ? (scale.value = 1.5) : (scale.value = 1)  
+  (scale.value === 1) ? 
+  (scale.value = withTiming(1.5, {duration: 500, easing: Easing.elastic(3)})) 
+  : 
+  (scale.value = withSpring(1));
   }
 
 
   return (
     <View style={styles.container}>
       <Animated.View style={[styles.element, , animatedStyle]} />
-      <Button       
+      <Button               
       title='Animar'  
       onPress={handlerAnimated}    
       />
